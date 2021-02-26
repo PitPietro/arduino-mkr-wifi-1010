bfc9778b22db77b77d792193849555af6a9d241c
pit <60300699+PitPietro@users.noreply.github.com>
2021-02-26 23:24:50 +0100

    Create musical-constants.h

---
+++ output/buzzer/play_melody/musical-constants.h
@@ -0,0 +1,17 @@
+// notes constants
+#define WHOLE 1
+#define HALF 0.5
+#define QUARTER 0.25
+#define EIGHTH 0.125
+#define SIXTEENTH 0.0625
+#define THIRTY_SECOND 0.03125
+#define SIXTY_FOURTH 0.015625
+
+// dotted constants starts with capital 'D'
+#define D_WHOLE 1.5
+#define D_HALF 0.75
+#define D_QUARTER 0.375
+#define D_EIGHTH 0.1875
+#define D_SIXTEENTH 0.09375
+#define D_THIRTY_SECOND 0.046875
+#define D_SIXTY_FOURTH 0.023438
