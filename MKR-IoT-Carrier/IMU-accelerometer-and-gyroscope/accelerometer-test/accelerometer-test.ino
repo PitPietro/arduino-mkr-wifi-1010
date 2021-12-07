e73b5d13381840b1df09c157d8c0d59cfed80718
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-12-07 16:13:28 +0100

    MKR-IoT-Carrier acceleration test

---
+++ MKR-IoT-Carrier/IMU-accelerometer-and-gyroscope/accelerometer-test/accelerometer-test.ino
@@ -0,0 +1,35 @@
+#include <Arduino_MKRIoTCarrier.h>
+
+MKRIoTCarrier carrier;
+float x, y, z;
+
+void setup() {
+  CARRIER_CASE = false;
+  carrier.begin();
+
+  // Initialize serial communication at 9600 baud rate
+  Serial.begin(9600);
+  // Wait for serial port to connect. Needed for native USB port only.
+  while (!Serial);
+}
+
+void loop() {
+  if (carrier.IMUmodule.accelerationAvailable()) {
+    carrier.IMUmodule.readAcceleration(x, y, z);
+    
+    // print the acceleration information on the serial monitor
+    printAcceleration();
+  }
+
+  // always place a short delay before closing the loop to let the board "take a breath" between the cycles.
+  delay(100);
+}
+
+void printAcceleration() {
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print(", y: ");
+  Serial.print(y);
+  Serial.print(", z: ");
+  Serial.println(z);
+}
